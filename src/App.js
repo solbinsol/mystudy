7d542ce04ce3f7916c6829e31c7afbfe17769caf
solbinsol <qkaejwnj@naver.com>
2023-03-21 23:01:35 +0900

    Input

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Routes,Route,Link} from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Counter from "./pages/Counter";
+import Input from "./pages/Input";
 //<Link> a 태그 같은것 사용하려면 임포트 두개 해야함
 function App() {
   return (
@@ -10,12 +11,14 @@ function App() {
       <nav>
         <Link to="/">Home</Link> | 
         <Link to="/about">About</Link> | 
-        <Link to="/counter">Counter</Link>
+        <Link to="/counter">Counter</Link> |
+        <Link to="/input">Input</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/about" element={<About />}/>
         <Route path="/counter" element={<Counter />}/>
+        <Route path="/input" element={<Input />}/>
       </Routes>
     </div>
   );
