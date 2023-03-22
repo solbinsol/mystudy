6407d10da5ad9ccd97711605cdbb5bef4c9f7180
solbinsol <qkaejwnj@naver.com>
2023-03-22 14:47:44 +0900

    sss

--- src/App.js
+++ src/App.js
@@ -1,30 +1,91 @@
 import React from "react";
-import {Routes,Route,Link} from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Counter from "./pages/Counter";
-import Input from "./pages/Input";
-import Input2 from "./pages/Input2";
+import './App.css';
+import { useEffect } from 'react';
+
+// import {Routes,Route,Link} from "react-router-dom";
+
+// import Kakao from "./kakao/Kakao";
+
+
+// import Home from "./pages/Home";
+// import About from "./pages/About";
+// import Counter from "./pages/Counter";
+// import Input from "./pages/Input";
+// import Input2 from "./pages/Input2";
+// import List from "./pages/List";
 //<Link> a 태그 같은것 사용하려면 임포트 두개 해야함
 function App() {
+//   return (
+//     <div className="App">
+// =
+
+//         <div className="App">
+//             <Kakao/>
+//         </div>
+//     </div>
+//   );
+  //스크립트 파일 읽어오기
+
+  
+
+
+  const new_script = src => { 
+    return new Promise((resolve, reject) => { 
+      const script = document.createElement('script'); 
+      script.src = src; 
+      script.addEventListener('load', () => { 
+        resolve(); 
+      }); 
+      script.addEventListener('error', e => { 
+        reject(e); 
+      }); 
+      document.head.appendChild(script); 
+    }); 
+  };
+  
+  useEffect(() => { 
+    //카카오맵 스크립트 읽어오기
+    const my_script = new_script('https://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=b6d8597b532512782b06bacbe71ad02f');
+    
+    //스크립트 읽기 완료 후 카카오맵 설정
+    my_script.then(() => { 
+      console.log('script loaded!!!');  
+      const kakao = window['kakao']; 
+      kakao.maps.load(() => {
+        const mapContainer = document.getElementById('map');
+        const options = { 
+          center: new kakao.maps.LatLng(37.70900421117985, 126.570667), //좌표설정
+          level: 13
+
+        };
+        
+        
+        const map = new kakao.maps.Map(mapContainer, options); //맵생성
+        //마커설정
+        // const markerPosition = new kakao.maps.LatLng(37.56000302825312, 126.97540593203321); 
+        // const marker = new kakao.maps.Marker({ 
+        //   position: markerPosition
+        // }); 
+        // marker.setMap(map); 
+      });   
+    }); 
+  }, []);
+
   return (
     <div className="App">
-      <nav>
-        <Link to="/">Home</Link> | 
-        <Link to="/about">About</Link> | 
-        <Link to="/counter">Counter</Link> |
-        <Link to="/input">Input</Link> |
-        <Link to="/input2">Input2</Link> 
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/about" element={<About />}/>
-        <Route path="/counter" element={<Counter />}/>
-        <Route path="/input" element={<Input />}/>
-        <Route path="/input2" element={<Input2 />}/>
-      </Routes>
+      <div className="mom">   
+            
+           <button className="sbtn" >start</button>
+
+
+           <div className="Start">
+      </div>
+
+      </div>
+      <div id="map" className="map"/>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/start/Start.js
@@ -0,0 +1,12 @@
+const Start = () =>{
+    <div style={{
+        width: '100%',
+        height:'100vh',
+        color: '#202b4257',
+        zIndex:'1'
+    }}>
+        <img src="logo512.png"></img>    
+    </div>
+}
+
+export default Start;
