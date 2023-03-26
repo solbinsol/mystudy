d6333ecc94085a4dbd00b3ebb6d8e8522a695424
solbinsol <qkaejwnj@naver.com>
2023-03-26 19:53:06 +0900

    sss

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useEffect } from 'react';
 import ChatGpt from  "./ChatGpt"
 import Detailview from "./Detailview"
 import Detail from "./Detail";
+import MarkClick from "./MarkClick";
 import {Routes,Route,Link, BrowserRouter} from "react-router-dom";
 
 // import Kakao from "./kakao/Kakao";
@@ -31,6 +32,8 @@ function App() {
     }); 
   };
   
+  
+
   useEffect(() => { 
     //카카오맵 스크립트 읽어오기
     const my_script = new_script('https://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=b6d8597b532512782b06bacbe71ad02f');
@@ -38,49 +41,65 @@ function App() {
     //스크립트 읽기 완료 후 카카오맵 설정
     my_script.then(() => { 
       console.log('script loaded!!!');  
-      const kakao = window['kakao']; 
+      var kakao = window['kakao']; 
       kakao.maps.load(() => {
-        const mapContainer = document.getElementById('map');
-        const options = { 
+        var mapContainer = document.getElementById('map');
+        var options = { 
           center: new kakao.maps.LatLng(37.62452719218404  ,127.66474549315299), //좌표설정
           level: 13
-
+          
         };
         
         
-        const map = new kakao.maps.Map(mapContainer, options); //맵생성
+        var map = new kakao.maps.Map(mapContainer, options); //맵생성
         //마커설정
-        // const markerPosition = new kakao.maps.LatLng(37.56000302825312, 126.97540593203321); 
-        // const marker = new kakao.maps.Marker({ 
-        //   position: markerPosition
-        // }); 
-        // marker.setMap(map); 
+        var markerPosition = new kakao.maps.LatLng(37.56000302825312, 126.97540593203321); 
+        var marker = new kakao.maps.Marker({ 
+        position: markerPosition
+        }); 
+        marker.setMap(map); 
+
+        kakao.maps.event.addListener(map, 'click', function(mouseEvent) {        
+    
+          // 클릭한 위도, 경도 정보를 가져옵니다 
+          var latlng = mouseEvent.latLng; 
+          
+          // 마커 위치를 클릭한 위치로 옮깁니다
+          marker.setPosition(latlng);
+          
+          
+        //   // 클릭 이벤트 관리
+          
+        //   var iwContent = '<div style="padding:5px;">Hello World!</div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+        //   iwRemoveable = true;
+        //   var infowindow = new kakao.maps.InfoWindow({
+        //     content : iwContent,
+        //     removable : iwRemoveable
+        // })
+        //   kakao.maps.event.addListener(marker, 'click', function() {
+        //     // 마커 위에 인포윈도우를 표시합니다
+        //     infowindow.open(map, marker);  
+        //   })
+          });
+      
       });   
     }); 
   }, []);
-
+  
+  
 
 
   return (
-
     <div className="App">
       <div className="mom">   
            <button className="sbtn" >start</button>
            <div className="Start">
       </div>
-
       </div>
       <div id="map" className="map"/>
-      
       <ChatGpt />
-      
       <Link to="/detail"><Detailview/></Link>
-
-
-
     </div>
-
-
   );
 }
 
