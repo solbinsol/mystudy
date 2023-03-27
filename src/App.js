934db2641287dfb361779fb10f03a8db47ea2aa5
solbinsol <qkaejwnj@naver.com>
2023-03-27 18:08:51 +0900

    멀쩡본

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { useEffect } from 'react';
 import ChatGpt from  "./ChatGpt"
 import Detailview from "./Detailview"
 import Detail from "./Detail";
-import MarkClick from "./MarkClick";
+
 import {Routes,Route,Link, BrowserRouter} from "react-router-dom";
 
 // import Kakao from "./kakao/Kakao";
@@ -53,6 +53,8 @@ function App() {
         
         var map = new kakao.maps.Map(mapContainer, options); //맵생성
         //마커설정
+
+        
         var markerPosition = new kakao.maps.LatLng(37.56000302825312, 126.97540593203321); 
         var marker = new kakao.maps.Marker({ 
         position: markerPosition
@@ -68,18 +70,18 @@ function App() {
           marker.setPosition(latlng);
           
           
-        //   // 클릭 이벤트 관리
+           // 클릭 이벤트 관리
           
-        //   var iwContent = '<div style="padding:5px;">Hello World!</div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-        //   iwRemoveable = true;
-        //   var infowindow = new kakao.maps.InfoWindow({
-        //     content : iwContent,
-        //     removable : iwRemoveable
-        // })
-        //   kakao.maps.event.addListener(marker, 'click', function() {
-        //     // 마커 위에 인포윈도우를 표시합니다
-        //     infowindow.open(map, marker);  
-        //   })
+           var iwContent = '<div style="padding:5px;">Hello World!</div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+           iwRemoveable = true;
+           var infowindow = new kakao.maps.InfoWindow({
+             content : iwContent,
+             removable : iwRemoveable
+         })
+           kakao.maps.event.addListener(marker, 'click', function() {
+             // 마커 위에 인포윈도우를 표시합니다
+             infowindow.open(map, marker);  
+           })
           });
       
       });   
