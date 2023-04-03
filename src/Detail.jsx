c7fe2dfdf0a425193d8ef58a98a3ed7307777c71
solbinsol <qkaejwnj@naver.com>
2023-04-03 15:16:58 +0900

    xxx

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -10,6 +10,60 @@ import sample4 from "./sample.jpg";
 
 const Detail = () =>{
   
+    const items = document.querySelectorAll('.slider-item');
+    const itemCount = items.length;
+    const nextItem = document.querySelector('.next');
+    const previousItem = document.querySelector('.previous');
+    const navItem = document.querySelector('a.toggle-nav');
+    let count = 0;
+    
+    function showNextItem() {
+      items[count].classList.remove('active');
+    
+      if(count < itemCount - 1) {
+        count++;
+      } else {
+        count = 0;
+      }
+    
+      items[count].classList.add('active');
+      console.log(count);
+    }
+    
+    function showPreviousItem() {
+      items[count].classList.remove('active');
+    
+      if(count > 0) {
+        count--;
+      } else {
+        count = itemCount - 1;
+      }
+    
+      items[count].classList.add('active');
+      // Check if working...
+      console.log(count);
+    }
+    
+    function toggleNavigation(){
+      this.nextElementSibling.classList.toggle('active');
+    }
+    
+    function keyPress(e) {
+      e = e || window.event;
+      
+      if (e.keyCode =='37') {
+        showPreviousItem();
+      } else if (e.keyCode == '39') {
+        showNextItem();
+      }
+    }
+    
+    nextItem.addEventListener('click', showNextItem);
+    previousItem.addEventListener('click', showPreviousItem);
+    document.addEventListener('keydown', keyPress);
+    navItem.addEventListener('click', toggleNavigation);
+    
+
     return(
       
     //   <div id = "root">
@@ -164,7 +218,10 @@ const Detail = () =>{
     //     </main>
 
     //     </div>
-      <html>
+      <html lang="en">
+        <head>
+        <meta charset="utf-8" />
+        </head>
               <body>
         <nav class="flex-nav">
   <div class="container">
@@ -310,7 +367,9 @@ const Detail = () =>{
     </div>
   </div>
 </div>
-</div>     
+</div>
+<script async src="./Dj.js">
+          </script>
       </body>
       </html>
 
